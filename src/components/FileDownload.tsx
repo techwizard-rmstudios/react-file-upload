026e0a8ff51cdc2f3dfbc3b7817fecf1a6c2df48
techwizard-rmstudios <techwizard.rmstudios@gmail.com>
2024-10-30 14:44:07 -0400

    complete: stage 2

--- src/components/FileDownload.tsx
+++ src/components/FileDownload.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { toast } from "react-toastify";
 import { baseUrl } from "config/const";
 import DownloadSVG from "elements/DownloadSVG";
+import Loading from "./Loading";
 
 interface Props {
   tab: string;
@@ -10,9 +11,13 @@ interface Props {
   index: string;
 }
 const FileDownload: React.FC<Props> = ({ tab, name, title, index }) => {
+  const [loading, setLoading] = useState(false);
+
   const downloadFile = () => {
+    setLoading(true);
     fetch(`${baseUrl}/api/download/${name}?tab=${tab}&index=${index}`)
       .then((response) => {
+        setLoading(false);
         if (response.status === 200) return response.blob();
         if (response.status === 404) throw new Error("File not found.");
         return response.json().then((errorData) => {
@@ -28,12 +33,15 @@ const FileDownload: React.FC<Props> = ({ tab, name, title, index }) => {
         link.click();
         link.remove();
       })
-      .catch((error) => toast.error(error?.message, { position: "top-right" }));
+      .catch((error) => {
+        setLoading(false);
+        toast.error(error?.message, { position: "top-right" });
+      });
   };
 
   return (
-    <button type="button" onClick={() => downloadFile()} className="flex items-center gap-2 py-8 px-8 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
-      {title} <DownloadSVG />
+    <button type="button" onClick={() => downloadFile()} disabled={loading} className="flex items-center gap-2 py-8 px-8 text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+      {title} {loading ? <Loading /> : <DownloadSVG />}
     </button>
   );
 };
--- src/pages/Stage2.tsx
+++ src/pages/Stage2.tsx
@@ -1,9 +1,46 @@
 import React from "react";
 import FileUpload from "components/FileUpload";
 import FileDownload from "components/FileDownload";
+import { dateString } from "config/const";
 
 const Stage2: React.FC = () => {
-  return <div className="dark flex flex-col mt-6"></div>;
+  return (
+    <div className="dark flex flex-col mt-6">
+      <div className="flex flex-col items-center sm:items-start justify-center sm:flex-row sm:flex-wrap gap-8 px-6 py-6 mx-auto">
+        <div>
+          <FileUpload key={1} name="2_CallEnquiryV2_Merged.xlsx" />
+          <p className="font-bold text-center text-white p-2">Reports billedCallEnquiryV2 (XLSX)</p>
+        </div>
+        <div>
+          <FileUpload key={2} name="2_Zoey Tax Report.xlsx" />
+          <p className="font-bold text-center text-white p-2">Zoey Tax Report (XLSX)</p>
+        </div>
+        <div>
+          <FileUpload key={3} name="2_ListOfServices.csv" />
+          <p className="font-bold text-center text-white p-2">Reports listOfservices (CSV)</p>
+        </div>
+        <div>
+          <FileUpload key={4} name="2_UnpostedFixedCharges.csv" />
+          <p className="font-bold text-center text-white p-2">Reports invoiceRun unpostedIrFixedCharges (CSV)</p>
+        </div>
+        <div>
+          <FileUpload key={5} name="2_Fixed Charges Report.xlsx" />
+          <p className="font-bold text-center text-white p-2">Fixed Charges Report (XLSX)</p>
+        </div>
+        <div>
+          <FileUpload key={6} name="2_Ceretax Item Codes.xlsx" />
+          <p className="font-bold text-center text-white p-2">Ceretax Item Codes (XLSX)</p>
+        </div>
+        <div>
+          <FileUpload key={7} name="2_Ceretax Address List.xlsx" />
+          <p className="font-bold text-center text-white p-2">Ceretax Address List (XLSX)</p>
+        </div>
+      </div>
+      <div className="flex items-center justify-center gap-8 px-6 py-6 mx-auto">
+        <FileDownload tab="2" name={`Ceretax Upload ${dateString()}.xlsx`} title="Download Ceretax Upload XLSX" index="1" />
+      </div>
+    </div>
+  );
 };
 
 export default Stage2;
